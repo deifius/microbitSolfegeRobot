ccafb67308125fcbd5ac049ad0ccf5a3e8266390
deifius <sunji.roaoul@gmail.com>
2022-04-23 07:28:08 -0600

    classified dances

--- sing_dance_map.py
+++ sing_dance_map.py
@@ -6,31 +6,31 @@ I am a set of relations of dance positions
 to sing actions
 """
 
+class dances:
+    def up():
+        print('yo!')
+        display.show(Image.ARROW_N)
+        sing(solfa[0], speed=100)
+        #sleep(1000)
+    def down():
+        display.show(Image.ARROW_S)
+        sing(solfa[1], speed=100)
+        #sleep(1000)
+    def right():
+        display.show(Image.ARROW_E)
+        sing(solfa[2], speed=100)
+        #sleep(1000)
+    def left():
+        display.show(Image.ARROW_W)
+        sing(solfa[3], speed=100)
+        #sleep(1000)
+    def face_up():
+        display.show(Image.CLOCK12)
+        sing(solfa[4], speed=100)
+        #sleep(1000)
+    def face_down():
+        display.show(Image.CLOCK6)
+        sing(solfa[5], speed=100)
+        #sleep(1000)
 
-def up():
-    display.show(Image.ARROW_N)
-    sing(solfa[0], speed=100)
-    #sleep(1000)
-def down():
-    display.show(Image.ARROW_S)
-    sing(solfa[1], speed=100)
-    #sleep(1000)
-def right():
-    display.show(Image.ARROW_E)
-    sing(solfa[2], speed=100)
-    #sleep(1000)
-def left():
-    display.show(Image.ARROW_W)
-    sing(solfa[3], speed=100)
-    #sleep(1000)
-def face_up():
-    display.show(Image.CLOCK12)
-    sing(solfa[4], speed=100)
-    #sleep(1000)
-def face_down():
-    display.show(Image.CLOCK6)
-    sing(solfa[5], speed=100)
-    #sleep(1000)
-
-gesture_map = {'up':up(),'down':down(),'right':right(),'left':left(),'face up':face_up(),'face down':face_down()}
-
+    dance = {'up':up,'down':down,'right':right,'left':left,'face up':face_up,'face down':face_down}
